c32972d96a350c244397cd427377c88388a32f09
attennig <g.attenni96@gmail.com>
2022-03-07 15:23:45 +0100

    python3

--- src/experiments/milp_experiments.sh
+++ src/experiments/milp_experiments.sh
@@ -1,7 +1,7 @@
 for n in "6" "8" "10" "12" "14" "16";
 do
     echo "generate size: ${n} "
-    python -m src.experiments.experiment -g -n ${n} -i_s 1 -e_s 6 -out ./out/milpexp -alg NONE &
+    python3 -m src.experiments.experiment -g -n ${n} -i_s 1 -e_s 6 -out ./out/milpexp -alg NONE &
     wait
 done;
 wait
@@ -10,9 +10,9 @@ do
     for method in "primal" "dual" "barrier" "concurrent";
     do
         echo "run: ${alg} - size: ${n} "
-        python -m src.experiments.experiment -n ${n} -i_s 1 -e_s 6 -out ./out/milpexp -alg MILP -method ${method} &
+        python3 -m src.experiments.experiment -n ${n} -i_s 1 -e_s 6 -out ./out/milpexp -alg MILP -method ${method} &
     done;
 done;
 wait
 
-python -m src.experiments.plots -out ./out/milpexp &
+python3 -m src.experiments.plots -out ./out/milpexp &
--- src/experiments/small_experiments.sh
+++ src/experiments/small_experiments.sh
@@ -1,7 +1,7 @@
 for n in "5" "6" "7" "8" "9" "10";
 do
     echo "generate size: ${n} "
-    python -m src.experiments.experiment -g -n ${n} -i_s 1 -e_s 6 -out ./out/small -alg NONE &
+    python3 -m src.experiments.experiment -g -n ${n} -i_s 1 -e_s 6 -out ./out/small -alg NONE &
     wait
 done;
 wait
@@ -11,9 +11,9 @@ do
     for alg in "MILP" "GREEDY";
     do
         echo "run: ${alg} - size: ${n} "
-        python -m src.experiments.experiment -n ${n} -i_s 1 -e_s 6 -out ./out/small -alg ${alg} &
+        python3 -m src.experiments.experiment -n ${n} -i_s 1 -e_s 6 -out ./out/small -alg ${alg} &
     done;
 done;
 wait
 
-python -m src.experiments.plots -out ./out/small &
+python3 -m src.experiments.plots -out ./out/small &
