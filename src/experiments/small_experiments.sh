c8814ccbeae0ac7481981c20251226e369fb63ca
attennig <g.attenni96@gmail.com>
2022-04-12 13:34:14 +0200

    milp+greedy+localsearch small

--- src/experiments/small_experiments.sh
+++ src/experiments/small_experiments.sh
@@ -1,19 +1,18 @@
-#for n in "6" "8" "10" "12";
-#do
-#    echo "generate size: ${n} "
-#    python -m src.experiments.experiment -g -n ${n} -i_s 1 -e_s 4 -out ./out/small2 -alg NONE &
-#    wait
-#done;
-#wait
-
-for n in "6" "8" "10";
+for n in "6" "8" "10" "12";
 do
-    for alg in "MILP" "GREEDY" "LOCALSEARCH";# "MILP" "GREEDY"
+    echo "generate size: ${n} "
+    python3 -m src.experiments.experiment -g -n ${n} -i_s 1 -e_s 4 -out ./out/smallC -alg NONE &
+    wait
+done;
+wait
+
+for n in "6" "8" "10" "12";
+do
+    for alg in "MILP" "GREEDY" "LOCALSEARCH-BFSOPT" "LOCALSEARCH-LB" "LOCALSEARCH-HC" ;
     do
         echo "run: ${alg} - size: ${n} "
-        python -m src.experiments.experiment -n ${n} -i_s 1 -e_s 4 -out ./out/small2 -alg ${alg} >> ./out/small2/sollog.txt 2>>./out/small2/sollog.txt
+        python3 -m src.experiments.experiment -n ${n} -i_s 1 -e_s 5 -out ./out/smallC -alg ${alg}
     done;
 done;
 wait
 
-#python -m src.experiments.plots -out ./out/small2 &
