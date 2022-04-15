b81d96d4218e702c8fad60244788373bf15fe631
Giulio Attenni <g.attenni96@gmail.com>
2022-04-15 19:44:59 +0200

    resultsU30D20M

--- src/experiments/M_experiments.sh
+++ src/experiments/M_experiments.sh
@@ -1,10 +1,10 @@
-for n in "20" "30" "40" "50" "60" "70" "80" "90";
-do
-    echo "generate size: ${n} "
-    python3 -m src.experiments.experiment -g -n ${n} -i_s 1 -e_s 6 -out ./out/M -alg NONE &
-    wait
-done;
-wait
+#for n in "20" "30" "40" "50" "60" "70" "80" "90";
+#do
+#    echo "generate size: ${n} "
+#    python3 -m src.experiments.experiment -g -n ${n} -i_s 1 -e_s 6 -out ./out/M -alg NONE &
+#    wait
+#done;
+#wait
 
 for n in "20" "30" "40" "50" "60" "70" "80" "90";
 do
