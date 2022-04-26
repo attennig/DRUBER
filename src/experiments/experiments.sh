9295d25e719c09f70e9cd520a097d9b54f285b13
attennig <g.attenni96@gmail.com>
2022-04-26 15:46:01 +0200

    Update experiments.sh

--- src/experiments/experiments.sh
+++ src/experiments/experiments.sh
@@ -1,16 +1,21 @@
-for ns in "5" "10";
+
+for nd in "4" "6";
 do
-  for nd in "2" "4" "6";
+  for nu in "4" "6" ;
   do
-    for nu in "2" "4" "6" ;
+    python3 -m src.experiments.simulate -ns 10 -nd ${nd} -nu ${nu} -i_s 1 -e_s 3 -out ./out -alg "MILP" -method "concurrent"&
+    python3 -m src.experiments.simulate -ns 10 -nd ${nd} -nu ${nu} -i_s 1 -e_s 3 -out ./out -alg "GREEDY" -method ""&
+    for method in "HC" "LB" "BFSOPT";
     do
-      python3 -m src.experiments.simulate -ns ${ns} -nd ${nd} -nu ${nu} -i_s 1 -e_s 3 -out ./out -alg "MILP" -method "concurrent"
-      python3 -m src.experiments.simulate -ns ${ns} -nd ${nd} -nu ${nu} -i_s 1 -e_s 3 -out ./out -alg "GREEDY" -method ""&
-      for method in "HC" "LB" "BFSOPT";
-      do
-        python3 -m src.experiments.simulate -ns ${ns} -nd ${nd} -nu ${nu} -i_s 1 -e_s 3 -out ./out -alg "LOCALSEARCH" -method ${method}&
-      done;
+      python3 -m src.experiments.simulate -ns 10 -nd ${nd} -nu ${nu} -i_s 1 -e_s 3 -out ./out -alg "LOCALSEARCH" -method ${method}&
     done;
   done;
 done;
+python3 -m src.experiments.simulate -ns 10 -nd 6 -nu 2 -i_s 1 -e_s 3 -out ./out -alg "MILP" -method "concurrent"&
+python3 -m src.experiments.simulate -ns 10 -nd 6 -nu 2 -i_s 1 -e_s 3 -out ./out -alg "GREEDY" -method ""&
+for method in "HC" "LB" "BFSOPT";
+do
+  python3 -m src.experiments.simulate -ns 10 -nd 6 -nu 2 -i_s 1 -e_s 3 -out ./out -alg "LOCALSEARCH" -method ${method}&
+done;
 wait
+
