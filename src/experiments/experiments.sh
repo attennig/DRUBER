31d7dc46cbe003f0a5e98530c035596f69522ea3
attennig <g.attenni96@gmail.com>
2022-04-26 16:03:08 +0200

    Update experiments.sh

--- src/experiments/experiments.sh
+++ src/experiments/experiments.sh
@@ -4,18 +4,8 @@ do
   for nu in "4" "6" ;
   do
     python3 -m src.experiments.simulate -ns 10 -nd ${nd} -nu ${nu} -i_s 1 -e_s 3 -out ./out -alg "MILP" -method "concurrent"&
-    python3 -m src.experiments.simulate -ns 10 -nd ${nd} -nu ${nu} -i_s 1 -e_s 3 -out ./out -alg "GREEDY" -method ""&
-    for method in "HC" "LB" "BFSOPT";
-    do
-      python3 -m src.experiments.simulate -ns 10 -nd ${nd} -nu ${nu} -i_s 1 -e_s 3 -out ./out -alg "LOCALSEARCH" -method ${method}&
-    done;
   done;
 done;
 python3 -m src.experiments.simulate -ns 10 -nd 6 -nu 2 -i_s 1 -e_s 3 -out ./out -alg "MILP" -method "concurrent"&
-python3 -m src.experiments.simulate -ns 10 -nd 6 -nu 2 -i_s 1 -e_s 3 -out ./out -alg "GREEDY" -method ""&
-for method in "HC" "LB" "BFSOPT";
-do
-  python3 -m src.experiments.simulate -ns 10 -nd 6 -nu 2 -i_s 1 -e_s 3 -out ./out -alg "LOCALSEARCH" -method ${method}&
-done;
 wait
 
