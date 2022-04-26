ca2525b8ee17642b34fc122aa34f8a0a69b200e6
Giulio <g.attenni96@gmail.com>
2022-04-26 20:27:15 +0200

    results

--- src/experiments/experiments.sh
+++ src/experiments/experiments.sh
@@ -4,10 +4,10 @@ do
   do
     for nu in "2" "4" "6" "8" "10" "12" "14" "16" "18" "20" "22" "24" "26" "28" "30";
     do
-      python3 -m src.experiments.simulate -ns 10 -nd ${nd} -nu ${nu} -i_s 1 -e_s 6 -out ./out -alg "GREEDY" -method ""&
+      python3 -m src.experiments.simulate -ns ${ns} -nd ${nd} -nu ${nu} -i_s 1 -e_s 6 -out ./out -alg "GREEDY" -method ""&
       for method in "HC" "LB" "BFSOPT";
       do
-        python3 -m src.experiments.simulate -ns 10 -nd ${nd} -nu ${nu} -i_s 1 -e_s 6 -out ./out -alg "LOCALSEARCH" -method ${method}&
+        python3 -m src.experiments.simulate -ns ${ns} -nd ${nd} -nu ${nu} -i_s 1 -e_s 6 -out ./out -alg "LOCALSEARCH" -method ${method}&
       done;
     done;
   done;
