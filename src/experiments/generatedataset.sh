d417aa49c17711dc171a5a3327d7a248458e673c
attennig <g.attenni96@gmail.com>
2022-05-03 10:30:40 +0200

    new experiments

--- src/experiments/generatedataset.sh
+++ src/experiments/generatedataset.sh
@@ -1,8 +1,8 @@
 for ns in "20";
 do
-  for nd in "5" "10" "15" "20" "25";
+  for nu in "5" "10" "15" "20" "25";
   do
-    for nu in "2" "4" "6" "8" "10" "12" "14" "16" "18" "20" "22" "24" "26" "28" "30";
+    for nd in "2" "4" "6" "8" "10" "12" "14" "16" "18" "20" "22" "24" "26" "28" "30";
     do
       python3 -m src.experiments.instancegenerator -ns ${ns} -nd ${nd} -nu ${nu} -i_s 1 -e_s 21 -out ./out &
       wait
