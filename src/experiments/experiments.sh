20c82c406496e2eed916d7e87aa5670fdac5fd0f
attennig <g.attenni96@gmail.com>
2022-05-25 17:09:01 +0200

    new exp 50seeds

--- src/experiments/experiments.sh
+++ src/experiments/experiments.sh
@@ -4,10 +4,10 @@ do
   do
     for nu in "2" "4" "6" "8" "10" "12" "14" "16" "18" "20" "22" "24" "26" "28" "30";
     do
-      python3 -m src.experiments.simulate -ns ${ns} -nd ${nd} -nu ${nu} -i_s 1 -e_s 11 -in ./in/S${ns} -out ./out -alg "GREEDY"&
+      python3 -m src.experiments.simulate -ns ${ns} -nd ${nd} -nu ${nu} -i_s 1 -e_s 51 -in ./in/S${ns} -out ./out -alg "GREEDY"&
       for method in "HC" "LB" "BFSOPT";
       do
-        python3 -m src.experiments.simulate -ns ${ns} -nd ${nd} -nu ${nu} -i_s 1 -e_s 11 -in ./in/S${ns}  -out ./out -alg "LOCALSEARCH" -method ${method}&
+        python3 -m src.experiments.simulate -ns ${ns} -nd ${nd} -nu ${nu} -i_s 1 -e_s 51 -in ./in/S${ns}  -out ./out -alg "LOCALSEARCH" -method ${method}&
       done;
     done;
   done;
