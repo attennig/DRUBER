8c1af5a863cd134355230e8aa21807887abd9050
attennig <g.attenni96@gmail.com>
2022-05-27 11:09:40 +0200

    experiments.sh update

--- src/experiments/experiments.sh
+++ src/experiments/experiments.sh
@@ -4,6 +4,7 @@ do
   do
     for nu in "2" "4" "6"; # "8" "10"; #"12" "14" "16" "18" "20" "22" "24" "26" "28" "30";
     do
+      mkdir ./out/S${ns}D${nd}U${nu}
       mkdir ./out/S${ns}D${nd}U${nu}/GREEDY
       python3 -m src.experiments.simulate -ns ${ns} -nd ${nd} -nu ${nu} -i_s 1 -e_s 11 -in ./in/S${ns} -out ./out -alg "GREEDY">./out/S${ns}D${nd}U${nu}/GREEDY/out.txt 2>./out/S${ns}D${nd}U${nu}/GREEDY/err.txt &
       for method in "HC" "LB" "BFSOPT";
