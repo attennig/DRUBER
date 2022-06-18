528f36cb3ba978edbbc3f2bf4fb11d0320d1d345
attennig <g.attenni96@gmail.com>
2022-06-18 11:26:18 +0200

    + primal - concurrent

--- src/experiments/experimentsMILP.sh
+++ src/experiments/experimentsMILP.sh
@@ -1,5 +1,5 @@
 mkdir ./out/Small/S5D2U2
-python3 -m src.experiments.simulate -ns 5 -nd 2 -nu 2 -i_s 9 -e_s 10 -in ./in/MILPexp -out ./out/Small -alg "MILP" -method "concurrent">./out/Small/S5D2U2MILPout.txt 2>./out/Small/S5D2U2MILPerr.txt &
+python3 -m src.experiments.simulate -ns 5 -nd 2 -nu 2 -i_s 9 -e_s 10 -in ./in/MILPexp -out ./out/Small -alg "MILP" -method "primal">./out/Small/S5D2U2MILPout.txt 2>./out/Small/S5D2U2MILPerr.txt &
 python3 -m src.experiments.simulate -ns 5 -nd 2 -nu 2 -i_s 9 -e_s 10 -in ./in/MILPexp -out ./out/Small -alg "GREEDY" >./out/Small/S5D2U2GREEDYout.txt 2>./out/Small/S5D2U2GREEDYerr.txt &
 for method in "HC" "LB" "BFSOPT";
     do
@@ -8,7 +8,7 @@ for method in "HC" "LB" "BFSOPT";
 
 
 mkdir ./out/Small/S5D2U3
-python3 -m src.experiments.simulate -ns 5 -nd 2 -nu 3 -i_s 10 -e_s 11 -in ./in/MILPexp -out ./out/Small -alg "MILP" -method "concurrent">./out/Small/S5D2U3MILPout.txt 2>./out/Small/S5D2U3MILPerr.txt &
+python3 -m src.experiments.simulate -ns 5 -nd 2 -nu 3 -i_s 10 -e_s 11 -in ./in/MILPexp -out ./out/Small -alg "MILP" -method "primal">./out/Small/S5D2U3MILPout.txt 2>./out/Small/S5D2U3MILPerr.txt &
 python3 -m src.experiments.simulate -ns 5 -nd 2 -nu 3 -i_s 10 -e_s 11 -in ./in/MILPexp -out ./out/Small -alg "GREEDY">./out/Small/S5D2U3GREEDYout.txt 2>./out/Small/S5D2U3GREEDYerr.txt &
 for method in "HC" "LB" "BFSOPT";
     do
@@ -17,7 +17,7 @@ for method in "HC" "LB" "BFSOPT";
 
 
 mkdir ./out/Small/S5D3U3
-python3 -m src.experiments.simulate -ns 5 -nd 3 -nu 3 -i_s 11 -e_s 12 -in ./in/MILPexp -out ./out/Small -alg "MILP" -method "concurrent">./out/Small/S5D3U3MILPout.txt 2>./out/Small/S5D3U3MILPerr.txt &
+python3 -m src.experiments.simulate -ns 5 -nd 3 -nu 3 -i_s 11 -e_s 12 -in ./in/MILPexp -out ./out/Small -alg "MILP" -method "primal">./out/Small/S5D3U3MILPout.txt 2>./out/Small/S5D3U3MILPerr.txt &
 python3 -m src.experiments.simulate -ns 5 -nd 3 -nu 3 -i_s 11 -e_s 12 -in ./in/MILPexp -out ./out/Small -alg "GREEDY">./out/Small/S5D3U3GREEDYout.txt 2>./out/Small/S5D3U3GREEDYerr.txt &
 for method in "HC" "LB" "BFSOPT";
     do
@@ -27,7 +27,7 @@ for method in "HC" "LB" "BFSOPT";
 
 
 mkdir ./out/Small/S5D3U4
-python3 -m src.experiments.simulate -ns 5 -nd 3 -nu 4 -i_s 12 -e_s 13 -in ./in/MILPexp -out ./out/Small -alg "MILP" -method "concurrent">./out/Small/S5D3U4MILPout.txt 2>./out/Small/S5D3U4MILPerr.txt &
+python3 -m src.experiments.simulate -ns 5 -nd 3 -nu 4 -i_s 12 -e_s 13 -in ./in/MILPexp -out ./out/Small -alg "MILP" -method "primal">./out/Small/S5D3U4MILPout.txt 2>./out/Small/S5D3U4MILPerr.txt &
 python3 -m src.experiments.simulate -ns 5 -nd 3 -nu 4 -i_s 12 -e_s 13 -in ./in/MILPexp -out ./out/Small -alg "GREEDY">./out/Small/S5D3U4GREEDYout.txt 2>./out/Small/S5D3U4GREEDYerr.txt &
 for method in "HC" "LB" "BFSOPT";
     do
@@ -36,7 +36,7 @@ for method in "HC" "LB" "BFSOPT";
 
 
 mkdir ./out/Small/S6D3U4
-python3 -m src.experiments.simulate -ns 6 -nd 3 -nu 4 -i_s 13 -e_s 14 -in ./in/MILPexp -out ./out/Small -alg "MILP" -method "concurrent">./out/Small/S6D3U4MILPout.txt 2>./out/Small/S6D3U4MILPerr.txt &
+python3 -m src.experiments.simulate -ns 6 -nd 3 -nu 4 -i_s 13 -e_s 14 -in ./in/MILPexp -out ./out/Small -alg "MILP" -method "primal">./out/Small/S6D3U4MILPout.txt 2>./out/Small/S6D3U4MILPerr.txt &
 python3 -m src.experiments.simulate -ns 6 -nd 3 -nu 4 -i_s 13 -e_s 14 -in ./in/MILPexp -out ./out/Small -alg "GREEDY">./out/Small/S6D3U4GREEDYout.txt 2>./out/Small/S6D3U4GREEDYerr.txt &
 for method in "HC" "LB" "BFSOPT";
     do
